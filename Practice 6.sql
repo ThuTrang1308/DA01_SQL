f8b89002960817818459e9e7a0943d7e6ea096f5
ThuTrang1308 <153346550+ThuTrang1308@users.noreply.github.com>
2023-12-19 19:30:22 +0700

    Update Practice 6.sql

--- Practice 6.sql
+++ Practice 6.sql
@@ -106,3 +106,10 @@ ORDER BY AVG(rating) DESC, title
 LIMIT 1)
 
 --ex12
+SELECT id, COUNT(*) AS num 
+FROM (SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id FROM RequestAccepted) AS friends_count
+GROUP BY id
+ORDER BY num DESC 
+LIMIT 1
